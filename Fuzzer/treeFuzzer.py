90dc0a62eddd2dbdb6357dbccc56042667077725
nnewram <marwenn02@gmail.com>
2020-08-27 17:11:23 +0200

    bruuuuh

--- Fuzzer/treeFuzzer.py
+++ Fuzzer/treeFuzzer.py
@@ -1,4 +1,5 @@
 import angr
+from CONFIG import *
 
 class Fuzzer:
     def __init__(self, entryTree, functionTrees, functionOffsets, binaryPath, auto_load_libs=False):
@@ -11,28 +12,37 @@ class Fuzzer:
         self.simulation_manager = self.project.factory.simulation_manager()
 
     def reachable(self, state, goal):
+        self.simulation_manager.drop(stash='active')
         self.simulation_manager.active = [state]
         self.simulation_manager.explore(find=goal)
         if self.simulation_manager.found:
-            return self.simulation_manager.found[-1]
+            return self.simulation_manager.found
         return False
 
     def evaluateBranch(self, state, branch):
         branchTup = branch.name
         if len(branchTup) == 2:
-            a = self.reachable(state, branchTup[0])
-            b = self.reachable(state, branchTup[1])
+            a = self.reachable(state.copy(), branchTup[0])
+            b = self.reachable(state.copy(), branchTup[1])
             if a and b:
-                return True
+                return [a, b]
             return a or b
         elif type(branchTup[2]) == str:
-            return self.evaluateFunctionCall(state, branchTup)
-        elif type(branchTup[2]) == UnconditionalRETURN:
-            return self.reachable(state, branchTup[0])
-        elif type(branchTup[2]) == UnconditionalJUMP:
-            a = self.reachable(state, branchTup[0])
-            
-
+            return self.evaluateFunctionCall(state.copy(), branchTup)
+        elif type(branchTup[2]) == URETURN:
+            return self.reachable(state.copy(), branchTup[0])
+        elif type(branchTup[2]) == UJUMP:
+            a = self.reachable(state.copy(), branchTup[0])
+            b = self.reachable(state.copy(), branchTup[1])
+            if a and b:
+                return [a, b]
+            return a or b
+        
+    def evaluateFunctionCall(self, state, branchTup):
+        for branch in self.functionTrees[branchTup[2]].children:
+            self.evaluateBranch(state, branch)
+    
     def fuzz(self):
         for child in self.entryTree.children:
-            self.evaluateBranch(self.state, child)
+            print(child)
+            print(self.evaluateBranch(self.project.factory.entry_state(), child))
